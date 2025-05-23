6b6c962d0f2fbbf369a49b8eb691579831ca96d3
Lexa22Flexa <165304856+Lexa22Flexa@users.noreply.github.com>
2025-05-23 21:51:46 +0200

    Začatí počítání těch kilometrů -> hází chybu, já už chtít mimir => dořešit zitra (já)

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\Race;
 use App\Models\RaceYear;
+use App\Models\Stage;
 use CodeIgniter\HTTP\ResponseInterface;
  
 
@@ -12,11 +13,13 @@ class Main extends BaseController
 {
     var $zavod;
     var $zavodyYear;
+    var $stage;
 
     public function __construct()
     {
         $this->zavod = new Race();
         $this->zavodyYear = new RaceYear();
+        $this->stage = new Stage();
     }
 
     public function index()
@@ -27,7 +30,12 @@ class Main extends BaseController
 
     public function zavod($idRace) {
         $data["zavodyYear"] = $this->zavodyYear->where("id_race", $idRace)->findAll();
-        
+        /*
+        $pocetKilometru = $this->stage->select("stage.distance, Count(*) as pocetKm")->join("race_year", "race_year.id = stage.id_race_year", "inner")->findAll();
+        $data["pocetKilometru"] = $pocetKilometru;
+        --> hází chybu, jdu už mimir, dobrou nooc :3
+        */
+
         echo view("jednotlivyZavod", $data);
     }
 
